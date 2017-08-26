ca8b1156a7b2de66cc0ae14fa90cfb9d788600dc
Freecoder <freecoder@naver.com>
2017-08-26 22:53:56 +0900

    my first commit

---
+++ bs.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+  </head>
+  <body>
+    <h1>Hello, Freecoder's Bootstrap</h1>
+
+	<!-- Stack the columns on mobile by making one full-width and the other half-width -->
+	<div class="row">
+	  <div class="col-12 col-md-8" style="background-color:pink;">.col-12 .col-md-8</div>
+	  <div class="col-6 col-md-4"  style="background-color:green;">.col-6 .col-md-4</div>
+	</div>
+	
+	<!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+	<div class="row">
+	  <div class="col-6 col-md-4" style="background-color:yellow;">.col-6 .col-md-4</div>
+	  <div class="col-6 col-md-4" style="background-color:lime;">.col-6 .col-md-4</div>
+	  <div class="col-6 col-md-4" style="background-color:fuchsia;">.col-6 .col-md-4</div>
+	</div>
+	
+	<!-- Columns are always 50% wide, on mobile and desktop -->
+	<div class="row">
+	  <div class="col-6"  style="background-color:silver;">.col-6</div>
+	  <div class="col-6"  style="background-color:purple;">.col-6</div>
+	</div>
+
+  </body>
+</html>
